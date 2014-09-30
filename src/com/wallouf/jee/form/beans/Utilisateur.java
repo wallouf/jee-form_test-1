ba3cb04051c9c178bf2c08628fe718cd6de0ade0
Wallouf <ct13860@gmail.com>
2014-09-30 18:42:30 -0400

    2nd step: use MVC
    Prepare beans

---
+++ src/com/wallouf/jee/form/beans/Utilisateur.java
@@ -0,0 +1,40 @@
+package com.wallouf.jee.form.beans;
+
+public class Utilisateur {
+
+    private String email;
+    private String motDePasse;
+    private String nom;
+
+    public Utilisateur( String email, String motDePasse, String nom ) {
+        super();
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail( String email ) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse( String motDePasse ) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom( String nom ) {
+        this.nom = nom;
+    }
+
+}
