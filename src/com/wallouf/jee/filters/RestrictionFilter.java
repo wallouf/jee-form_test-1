b1f1290c53a9a5bdaa1c36841e4b075aaf7ec6b1
Wallouf <ct13860@gmail.com>
2014-10-05 15:08:06 -0400

    4 part : test filters

---
+++ src/com/wallouf/jee/filters/RestrictionFilter.java
@@ -0,0 +1,65 @@
+package com.wallouf.jee.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class RestrictionFilter
+ */
+@WebFilter( "/RestrictionFilter" )
+public class RestrictionFilter implements Filter {
+
+    public static final String ACCES_PUBLIC     = "/accesPublic.jsp";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+
+    /**
+     * Default constructor.
+     */
+    public RestrictionFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter( ServletRequest request, ServletResponse response, FilterChain chain ) throws IOException,
+            ServletException {
+        // TODO Auto-generated method stub
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+
+        if ( session.getAttribute( ATT_SESSION_USER ) == null ) {
+            res.sendRedirect( req.getContextPath() + ACCES_PUBLIC );
+        } else {
+            // pass the request along the filter chain
+            chain.doFilter( request, response );
+        }
+
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init( FilterConfig fConfig ) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
