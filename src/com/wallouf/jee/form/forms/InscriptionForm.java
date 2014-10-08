a05e92aa8f4c5d5eec4ca5647063da8c555dea01
Wallouf <ct13860@gmail.com>
2014-10-08 17:27:00 -0400

    Part 5 : Use DAO & JDBA for data

--- src/com/wallouf/jee/form/forms/InscriptionForm.java
+++ src/com/wallouf/jee/form/forms/InscriptionForm.java
@@ -5,16 +5,22 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.jasypt.util.password.ConfigurablePasswordEncryptor;
+
+import com.wallouf.jee.dao.DAOException;
+import com.wallouf.jee.dao.UtilisateurDao;
 import com.wallouf.jee.form.beans.Utilisateur;
 
 public class InscriptionForm {
-    private static final String CHAMP_EMAIL = "email";
-    private static final String CHAMP_PASS  = "motdepasse";
-    private static final String CHAMP_CONF  = "confirmation";
-    private static final String CHAMP_NOM   = "nom";
+    private static final String CHAMP_EMAIL      = "email";
+    private static final String CHAMP_PASS       = "motdepasse";
+    private static final String CHAMP_CONF       = "confirmation";
+    private static final String CHAMP_NOM        = "nom";
+    private static final String ALGO_CHIFFREMENT = "SHA-256";
 
     private String              resultat;
-    private Map<String, String> erreurs     = new HashMap<String, String>();
+    private Map<String, String> erreurs          = new HashMap<String, String>();
+    private UtilisateurDao      utilisateurDao;
 
     public String getResultat() {
         return resultat;
@@ -24,6 +30,10 @@ public class InscriptionForm {
         return erreurs;
     }
 
+    public InscriptionForm( UtilisateurDao utilisateurDao ) {
+        this.utilisateurDao = utilisateurDao;
+    }
+
     public Utilisateur inscrireUtilisateur( HttpServletRequest request ) {
         String email = getValeurChamp( request, CHAMP_EMAIL );
         String motDePasse = getValeurChamp( request, CHAMP_PASS );
@@ -31,63 +41,110 @@ public class InscriptionForm {
         String nom = getValeurChamp( request, CHAMP_NOM );
 
         Utilisateur utilisateur = new Utilisateur();
-
         try {
-            validationEmail( email );
-        } catch ( Exception e ) {
-            setErreur( CHAMP_EMAIL, e.getMessage() );
-        }
-        utilisateur.setEmail( email );
+            traiterEmail( email, utilisateur );
+            traiterMotsDePasse( motDePasse, confirmation, utilisateur );
+            traiterNom( nom, utilisateur );
 
-        try {
-            validationMotsDePasse( motDePasse, confirmation );
-        } catch ( Exception e ) {
-            setErreur( CHAMP_PASS, e.getMessage() );
-            setErreur( CHAMP_CONF, null );
-        }
-        utilisateur.setMotDePasse( motDePasse );
-
-        try {
-            validationNom( nom );
-        } catch ( Exception e ) {
-            setErreur( CHAMP_NOM, e.getMessage() );
-        }
-        utilisateur.setNom( nom );
-
-        if ( erreurs.isEmpty() ) {
-            resultat = "Succès de l'inscription.";
-        } else {
-            resultat = "Échec de l'inscription.";
+            if ( erreurs.isEmpty() ) {
+                utilisateurDao.creer( utilisateur );
+                resultat = "Succès de l'inscription.";
+            } else {
+                resultat = "Échec de l'inscription.";
+            }
+        } catch ( DAOException e ) {
+            resultat = "Échec de l'inscription : une erreur imprévue est survenue, merci de réessayer dans quelques instants.";
+            e.printStackTrace();
         }
 
         return utilisateur;
     }
 
-    private void validationEmail( String email ) throws Exception {
+    /*
+     * Appel à la validation de l'adresse email reçue et initialisation de la
+     * propriété email du bean
+     */
+    private void traiterNom( String nom, Utilisateur utilisateur ) {
+        try {
+            validationNom( nom );
+        } catch ( FormValidationException e ) {
+            setErreur( CHAMP_EMAIL, e.getMessage() );
+        }
+        utilisateur.setNom( nom );
+    }
+
+    /*
+     * Appel à la validation de l'adresse email reçue et initialisation de la
+     * propriété email du bean
+     */
+    private void traiterEmail( String email, Utilisateur utilisateur ) {
+        try {
+            validationEmail( email );
+        } catch ( FormValidationException e ) {
+            setErreur( CHAMP_EMAIL, e.getMessage() );
+        }
+        utilisateur.setEmail( email );
+    }
+
+    /*
+     * Appel à la validation des mots de passe reçus, chiffrement du mot de
+     * passe et initialisation de la propriété motDePasse du bean
+     */
+    private void traiterMotsDePasse( String motDePasse, String confirmation, Utilisateur utilisateur ) {
+        try {
+            validationMotsDePasse( motDePasse, confirmation );
+        } catch ( FormValidationException e ) {
+            setErreur( CHAMP_PASS, e.getMessage() );
+            setErreur( CHAMP_CONF, null );
+        }
+
+        /*
+         * Utilisation de la bibliothèque Jasypt pour chiffrer le mot de passe
+         * efficacement.
+         * 
+         * L'algorithme SHA-256 est ici utilisé, avec par défaut un salage
+         * aléatoire et un grand nombre d'itérations de la fonction de hashage.
+         * 
+         * La String retournée est de longueur 56 et contient le hash en Base64.
+         */
+        ConfigurablePasswordEncryptor passwordEncryptor = new ConfigurablePasswordEncryptor();
+        passwordEncryptor.setAlgorithm( ALGO_CHIFFREMENT );
+        passwordEncryptor.setPlainDigest( false );
+        String motDePasseChiffre = passwordEncryptor.encryptPassword( motDePasse );
+
+        utilisateur.setMotDePasse( motDePasseChiffre );
+    }
+
+    /* Validation de l'adresse email */
+    private void validationEmail( String email ) throws FormValidationException {
         if ( email != null ) {
             if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
-                throw new Exception( "Merci de saisir une adresse mail valide." );
+                throw new FormValidationException( "Merci de saisir une adresse mail valide." );
+            } else if ( utilisateurDao.trouver( email ) != null ) {
+                throw new FormValidationException(
+                        "Cette adresse email est déjà utilisée, merci d'en choisir une autre." );
             }
         } else {
-            throw new Exception( "Merci de saisir une adresse mail." );
+            throw new FormValidationException( "Merci de saisir une adresse mail." );
         }
     }
 
-    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception {
+    private void validationMotsDePasse( String motDePasse, String confirmation ) throws FormValidationException {
         if ( motDePasse != null && confirmation != null ) {
             if ( !motDePasse.equals( confirmation ) ) {
-                throw new Exception( "Les mots de passe entrés sont différents, merci de les saisir à nouveau." );
+                throw new FormValidationException(
+                        "Les mots de passe entrés sont différents, merci de les saisir à nouveau." );
             } else if ( motDePasse.length() < 3 ) {
-                throw new Exception( "Les mots de passe doivent contenir au moins 3 caractères." );
+                throw new FormValidationException( "Les mots de passe doivent contenir au moins 3 caractères." );
             }
         } else {
-            throw new Exception( "Merci de saisir et confirmer votre mot de passe." );
+            throw new FormValidationException( "Merci de saisir et confirmer votre mot de passe." );
         }
     }
 
-    private void validationNom( String nom ) throws Exception {
+    private void validationNom( String nom ) throws FormValidationException {
         if ( nom != null && nom.length() < 3 ) {
-            throw new Exception( "Le nom d'utilisateur doit contenir au moins 3 caractères." );
+            throw new FormValidationException( "Le nom d'utilisateur doit contenir au moins 3 caractères." );
         }
     }
 
--- src/com/wallouf/jee/form/servlets/Inscription.java
+++ src/com/wallouf/jee/form/servlets/Inscription.java
@@ -3,57 +3,44 @@ package com.wallouf.jee.form.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.wallouf.jee.dao.DAOFactory;
+import com.wallouf.jee.dao.UtilisateurDao;
 import com.wallouf.jee.form.beans.Utilisateur;
 import com.wallouf.jee.form.forms.InscriptionForm;
 
-/**
- * Servlet implementation class Inscription
- */
-@WebServlet( "/Inscription" )
 public class Inscription extends HttpServlet {
-    public static final String vue              = "/WEB-INF/inscription.jsp";
+    public static final String CONF_DAO_FACTORY = "daofactory";
     public static final String ATT_USER         = "utilisateur";
     public static final String ATT_FORM         = "form";
+    public static final String VUE              = "/WEB-INF/inscription.jsp";
 
-    private static final long  serialVersionUID = 1L;
+    private UtilisateurDao     utilisateurDao;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Inscription() {
-        super();
-        // TODO Auto-generated constructor stub
+    public void init() throws ServletException {
+        /* Récupération d'une instance de notre DAO Utilisateur */
+        this.utilisateurDao = ( (DAOFactory) getServletContext().getAttribute( CONF_DAO_FACTORY ) ).getUtilisateurDao();
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException,
-            IOException {
-        // TODO Auto-generated method stub
-        this.getServletContext().getRequestDispatcher( vue ).forward( request, response );
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Affichage de la page d'inscription */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException,
-            IOException {
-        // TODO Auto-generated method stub
-        InscriptionForm form = new InscriptionForm();
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Préparation de l'objet formulaire */
+        InscriptionForm form = new InscriptionForm( utilisateurDao );
+
+        /* Traitement de la requête et récupération du bean en résultant */
         Utilisateur utilisateur = form.inscrireUtilisateur( request );
 
-        request.setAttribute( ATT_USER, utilisateur );
+        /* Stockage du formulaire et du bean dans l'objet request */
         request.setAttribute( ATT_FORM, form );
+        request.setAttribute( ATT_USER, utilisateur );
 
-        this.getServletContext().getRequestDispatcher( vue ).forward( request, response );
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
-
 }
